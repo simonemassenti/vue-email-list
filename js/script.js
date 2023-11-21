41637182a6d664952dec8cebba36a6aa70435157
simonemassenti <s.massenti96@gmail.com>
2023-11-21 16:28:35 +0100

    doc: js document

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@ const app = createApp({
         }
     },
     created() {
+        //For loop that runs 10 times
         for (let i = 0; i < 10; i++) {
             axios
+            //Get the information, in this case get a random email
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            //Response management 
                 .then((resp) => {
+                    //The email is added to the array
                     this.emails[i] = resp.data.response;
                 });
         }
